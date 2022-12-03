d321f08527a68899aaa203557f56107c7ce75b8c
NaibOfDesert <tryc.artur@gmail.com>
2022-12-03 14:08:00 +0100

    Space ships.

---
+++ CommonAirFighter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal"); 
+        float verticalThrow = Input.GetAxis("Vertical"); 
+        Debug.Log(horizontalThrow);
+    }
+
+
+}
