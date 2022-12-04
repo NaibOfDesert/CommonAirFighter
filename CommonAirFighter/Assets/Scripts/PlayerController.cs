bd5c348c5a30b02f0bc820264fc5cdb51ecdb2c0
NaibOfDesert <tryc.artur@gmail.com>
2022-12-04 11:56:51 +0100

    New input system.

--- CommonAirFighter/Assets/Scripts/PlayerController.cs
+++ CommonAirFighter/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    [SerializeField] InputAction fire;
+
     void Start()
     {
         
     }
-
+    private void OnEnable()
+    {
+        movement.Enable();   
+    }
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal"); 
-        float verticalThrow = Input.GetAxis("Vertical"); 
+        /*float horizontalThrow = Input.GetAxis("Horizontal");
         Debug.Log(horizontalThrow);
+        
+        float verticalThrow = Input.GetAxis("Vertical"); 
+        Debug.Log(verticalThrow);*/
+
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        Debug.Log(horizontalThrow);
+
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        Debug.Log(verticalThrow);
+
     }
 
 
