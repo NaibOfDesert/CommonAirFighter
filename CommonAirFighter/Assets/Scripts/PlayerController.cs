c2ea5141da11ee85c8c71580aa06071264d7b270
NaibOfDesert <tryc.artur@gmail.com>
2022-12-04 14:58:38 +0100

    Player Controller.

--- CommonAirFighter/Assets/Scripts/PlayerController.cs
+++ CommonAirFighter/Assets/Scripts/PlayerController.cs
@@ -1,40 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+// using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
-    [SerializeField] InputAction fire;
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 2.5f;
+    [SerializeField] float controlRollFactor = 5f;
+
+    float xThrow = 0f;
+    float yThrow = 0f;
+
+    // [SerializeField] InputAction movement;
+    // [SerializeField] InputAction fire;
 
     void Start()
     {
         
     }
-    private void OnEnable()
+   /* private void OnEnable()
     {
         movement.Enable();   
     }
     private void OnDisable()
     {
         movement.Disable();
-    }
+    }*/
     void Update()
     {
-        /*float horizontalThrow = Input.GetAxis("Horizontal");
+        ProcessTranslation();
+        ProcessRotation(); 
+    }
+    void ProcessTranslation()
+    {
+        /*float horizontalThrow = movement.ReadValue<Vector2>().x;
         Debug.Log(horizontalThrow);
-        
-        float verticalThrow = Input.GetAxis("Vertical"); 
+        float verticalThrow = movement.ReadValue<Vector2>().y;
         Debug.Log(verticalThrow);*/
 
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        Debug.Log(horizontalThrow);
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-        Debug.Log(verticalThrow);
+        float xOffSet = xThrow * Time.deltaTime * controlSpeed;
+        float yOffSet = yThrow * Time.deltaTime * controlSpeed;
 
+
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float rawYPos = transform.localPosition.y + yOffSet;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor ; 
+        float roll = xThrow * controlRollFactor; 
 
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
 }
